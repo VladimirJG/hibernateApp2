103a8cba452a1367f3a51d858e5b13735ca05781
VladimirJG <vladimi85@gmail.com>
2023-11-08 12:12:12 +0300

    Initial commit

---
+++ src/main/java/crud/hiber/App.java
@@ -0,0 +1,28 @@
+package crud.hiber;
+
+import crud.hiber.model.Item;
+import crud.hiber.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Item.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}
